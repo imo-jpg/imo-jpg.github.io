af2b523ffa5197d6d7cfba93068a56d3b49bacf1
Imogen Todd <imo.todd@gmail.com>
2020-08-12 22:50:51 -0400

    tries for mobile touch event

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let links = document.getElementsByClassName("link");
 
 for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function showPreview(event) {
-        let element = event.target;
-        let preview = event.target.nextSibling.nextSibling;
+    links[i].addEventListener("click", showPreview);
+    links[i].addEventListener("touchstart", showPreview);
+}
 
-        if (element.classList.contains("open") == false) {
-            preview.classList.add("show");
-            event.target.classList.add("open");
+function showPreview(event) {
+    let element = event.target;
+    let preview = event.target.nextSibling.nextSibling;
 
-        } else {
-            preview.classList.remove("show");
-            event.target.classList.remove("open");
-        }
+    if (element.classList.contains("open") == false) {
+        preview.classList.add("show");
+        event.target.classList.add("open");
 
-    });
+    } else {
+        preview.classList.remove("show");
+        event.target.classList.remove("open");
+    }
 }
 
